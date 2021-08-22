ecb5fb57c0eba1153c2e9714979888518edeb88e
Elliot <w06148eb@student.manchester.ac.uk>
2021-08-22 18:50:54 +0100

    Check3x3Duplicate works

--- sudoku solver/src/structure/SuperGrid.java
+++ sudoku solver/src/structure/SuperGrid.java
@@ -42,11 +42,10 @@ public class SuperGrid {
 
     public List<Integer> getColumn(int index) {
         List<Integer> columnList = new ArrayList<Integer>();
-        columnList.add(tiles.get(0).get(index).getVal());
-        columnList.add(tiles.get(1).get(index).getVal());
-        columnList.add(tiles.get(2).get(index).getVal());
+        for (int i = 0; i < 9 ; i++) {
+            columnList.add(tiles.get(i).get(index).getVal());
 
-        // needs to get all rows now, not just 3
+        }
 
         return columnList;
     }
@@ -60,7 +59,7 @@ public class SuperGrid {
         tiles.get(y).set(x, new Tile(value));
     }
 
-    public boolean checkSet(Set<Integer> uncheckedSet) {
+    public boolean validSet(Set<Integer> uncheckedSet) {
 
         if (uncheckedSet.contains(1) && uncheckedSet.contains(2) && uncheckedSet.contains(3) && uncheckedSet.contains(4) && uncheckedSet.contains(5) && uncheckedSet.contains(6) && uncheckedSet.contains(7) && uncheckedSet.contains(8) && uncheckedSet.contains(9)) {
             if (uncheckedSet.size() == 9) {
@@ -78,50 +77,31 @@ public class SuperGrid {
 
 
     public boolean Check3x3Duplicates() {
+
         Set<Integer> gridSet;
-        for (int GridStartY = 0; GridStartY < 6; GridStartY += 3) {
-            for (int GridStartX = 0; GridStartX < 6; GridStartX += 3) {
+        
+        for (int GridStartY = 0; GridStartY < 9; GridStartY += 3) {
+            for (int GridStartX = 0; GridStartX < 9; GridStartX += 3) {
                 gridSet = new HashSet<>();
                 // creates new set for each grid
-                for (int y = 0; y < 2; y++) {
-                    for (int x = 0; x < 2; x++) {
+                for (int y = 0; y < 3; y++) {
+                    for (int x = 0; x < 3; x++) {
 
                         gridSet.add(tiles.get(GridStartY + y).get(GridStartX + x).getVal());
                         // adds all cells in the grid into a set to be checked for duplicates
 
                     }
                 }
-
-                if (!checkSet(gridSet)) {
-                    return false;
+                System.out.print(gridSet);
+                if (!validSet(gridSet)) {
+                    return true;
                 }
             
             }
         }
-        return true;
+        return false;
+        // no duplicates found
     }
-/** NEEDS REAVALUATING FOR 9X9 has to check for grid duplicates
-    public boolean check3x3Duplicates() {
-        Set<Integer> checkSet = new HashSet<>();
-        for (List<Tile> row : tiles) {
-            for (Tile tile : row) {
-                if (!tile.isEmpty()){
-                    checkSet.add(tile.getVal());
-                }
-                
-            }
-            
-        }
-        System.out.println(checkSet.toString());
-        if (checkSet.size() == 9) {
-            return false;
-        }
-        else {
-            return true;
-        }
-
-    }
-    **/
 
     public String toString() {
         String stringBuilder = "-------------------------------------\n";
--- sudoku solver/src/sudoku.java
+++ sudoku solver/src/sudoku.java
@@ -23,12 +23,27 @@ public class sudoku {
         };
 
 
+        Integer[][] solvedSudoku = new Integer[][]{
+            { 1, 2, 3, 6, 7, 8, 9, 4, 5 },
+            { 5, 8, 4, 2, 3, 9, 7, 6, 1 },
+            { 9, 6, 7, 1, 4, 5, 3, 2, 8 },
+            { 3, 7, 2, 4, 6, 1, 5, 8, 9 },
+            { 6, 9, 1, 5, 8, 3, 2, 7, 4 },
+            { 4, 5, 8, 7, 9, 2, 6, 1, 3 },
+            { 8, 3, 6, 9, 2, 4, 1, 5, 7 },
+            { 2, 1, 9, 8, 5, 7, 4, 3, 6 },
+            { 7, 4, 5, 3, 1, 6, 8, 9, 2 }
+
+        };
+
+
 
         // add these rows to create a test grid to check the toString function
-        SuperGrid testGrid = SuperGrid.patternToGrid(pattern);
+        SuperGrid testGrid = SuperGrid.patternToGrid(solvedSudoku);
 
 
         System.out.println(testGrid.toString());
+        System.out.println(testGrid.Check3x3Duplicates());
     }
     
 
