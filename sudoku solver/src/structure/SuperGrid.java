6ebb06c592a17ca09b36b7bea700c8fefbee721c
Elliot <w06148eb@student.manchester.ac.uk>
2021-08-22 12:20:44 +0100

    checks for duplicates checks 3x3 grids for duplicates but has not been tested yet

--- sudoku solver/src/structure/SuperGrid.java
+++ sudoku solver/src/structure/SuperGrid.java
@@ -12,16 +12,6 @@ public class SuperGrid {
     public SuperGrid(List<List<Tile>> newTiles) {
         tiles = newTiles;
     } 
-    
-    public List<Integer> getRow(int index) {
-        List<Tile> rowListTiles = tiles.get(index);
-        List<Integer> rowList = new ArrayList<>();
-        for (Tile tile : rowListTiles) {
-            rowList.add(tile.getVal());
-            
-        }
-        return rowList;
-    }
 
 
     public static SuperGrid patternToGrid(Integer[][] pattern) {
@@ -39,11 +29,25 @@ public class SuperGrid {
         return new SuperGrid(tileArray);
     }
 
+        
+    public List<Integer> getRow(int index) {
+        List<Tile> rowListTiles = tiles.get(index);
+        List<Integer> rowList = new ArrayList<>();
+        for (Tile tile : rowListTiles) {
+            rowList.add(tile.getVal());
+            
+        }
+        return rowList;
+    }
+
     public List<Integer> getColumn(int index) {
         List<Integer> columnList = new ArrayList<Integer>();
         columnList.add(tiles.get(0).get(index).getVal());
         columnList.add(tiles.get(1).get(index).getVal());
         columnList.add(tiles.get(2).get(index).getVal());
+
+        // needs to get all rows now, not just 3
+
         return columnList;
     }
 
@@ -54,6 +58,47 @@ public class SuperGrid {
 
     public void setTile(int x, int y, int value){
         tiles.get(y).set(x, new Tile(value));
+    }
+
+    public boolean checkSet(Set<Integer> uncheckedSet) {
+
+        if (uncheckedSet.contains(1) && uncheckedSet.contains(2) && uncheckedSet.contains(3) && uncheckedSet.contains(4) && uncheckedSet.contains(5) && uncheckedSet.contains(6) && uncheckedSet.contains(7) && uncheckedSet.contains(8) && uncheckedSet.contains(9)) {
+            if (uncheckedSet.size() == 9) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        else {
+            return false;
+        }
+
+    }
+
+
+    public boolean Check3x3Duplicates() {
+        Set<Integer> gridSet;
+        for (int GridStartY = 0; GridStartY < 6; GridStartY += 3) {
+            for (int GridStartX = 0; GridStartX < 6; GridStartX += 3) {
+                gridSet = new HashSet<>();
+                // creates new set for each grid
+                for (int y = 0; y < 2; y++) {
+                    for (int x = 0; x < 2; x++) {
+
+                        gridSet.add(tiles.get(GridStartY + y).get(GridStartX + x).getVal());
+                        // adds all cells in the grid into a set to be checked for duplicates
+
+                    }
+                }
+
+                if (!checkSet(gridSet)) {
+                    return false;
+                }
+            
+            }
+        }
+        return true;
     }
 /** NEEDS REAVALUATING FOR 9X9 has to check for grid duplicates
     public boolean check3x3Duplicates() {
